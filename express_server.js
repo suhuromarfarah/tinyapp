70dad3e10ee7f2f74d8d26381269f6ad9efb2440
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-18 18:57:42 +0000

    url shortening

--- express_server.js
+++ express_server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
-const PORT = 5000; // default port 8080
+const PORT = 3000; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+const generateRandomString = () => {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var numOfChar = characters.length;
+  for ( let i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * numOfChar));
+  }
+  return result;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,6 +26,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -21,15 +39,28 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`urls/${shortURL}`)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  shortURL = req.params.shortURL;
+  if(urlDatabase.hasOwnProperty(shortURL)) {
+    let longURL = urlDatabase[shortURL];
+    res.redirect(`${longURL}`);
+  };
 });
 
 app.listen(PORT, () => {
