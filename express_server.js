f60e25094deb7b6d3f02ba440e96a9d673316f36
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-20 20:50:52 +0000

    most thing login & registration .. fix register

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 3000; // default port 8080
+const PORT = 5000; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 let cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+const users = {}
 
 app.set("view engine", "ejs");
 
@@ -25,20 +27,34 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+function checkUserEmail(emailAddress) {
+  for (const user in users) {
+    if (users[user].email === emailAddress) {
+      return true;
+    }
+  }
+  return false;
+};
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+app.get("/users.json", (req, res) => {
+  res.json(users);
 });
 
 app.get("/urls", (req, res) => { //changing this to:
-  let templateVars = {
+  const user = users[req.cookies["user_id"]];
+  if (user) {
+    let templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
-  }
+    user
+  };
   res.render("urls_index", templateVars);
+} else {
+  res.redirect("/register");
+}
 });
 
 // app.get("/hello", (req, res) => {
@@ -46,15 +62,20 @@ app.get("/urls", (req, res) => { //changing this to:
 // });
 
 app.get("/urls/new", (req, res) => {
+  // let templateVars = {
+  //   user: req.cookies['user']
+  // };
+  // res.render("urls_new", templateVars);
+  const user = users[req.cookies["user_id"]];
   let templateVars = {
-    username: req.cookies['username']
+    user
   };
-  res.render("urls_new", templateVars);
+  res.render("urls_new", templateVars)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: req.cookies["user"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
@@ -67,9 +88,38 @@ app.post("/urls", (req, res) => {
   res.redirect(`urls/${shortURL}`)
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const user = users[req.cookies["user_id"]];
+  let templateVars = {
+    user,
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.get("/register", (req, res) => {
+  res.render("urls_register", { user: undefined });
+});
+
+app.post("/register", (req, res) => {
+  if(checkUserEmail(req.body.email)) {
+    res.sendStatus(400);
+  }
+  console.log(Object.values(users));
+  const id = generateRandomString();
+  users[id] = {id, email: req.body.email, password: req.body.password};
+  if (users[id].email === "" || users[id].password === "") {
+    res.send(`Enter your email & password. Statuscode:${res.sendStatus(400)}`);
+  } else {
+    res.cookie("user_id", users[id].id);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -77,14 +127,26 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
 })
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
+app.get("/login", (req, res) => {
+  res.render("urls_login", {user: undefined});
 });
 
+app.post("/login", (req, res) => {
+  if (checkUsersEmail(req.body.email)) {
+    for (let id in users) {
+      if (users[id].email === req.body.email && users[id].password === req.body.password) {
+        res.cookie("user_id", users[id].id);
+        res.redirect("/urls");
+      }
+    }
+  }
+  res.sendStatus(403);
+});
+
+
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+  res.clearCookie("user_id", users[req.cookies["user_id"]]);
+  res.redirect("/login");
 })
 
 app.listen(PORT, () => {
